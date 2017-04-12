91c3647bc225c1f553cbeb7641b2891cb90a6da0
s7lx <s7lxhacker@gmail.com>
2017-04-13 00:32:39 +0800

    ignore *.db*

--- run.py
+++ run.py
@@ -5,36 +5,40 @@ import sys
 import time
 from datetime import datetime
 
-def unixstamp2strtime(unixstamp)
-'''
-    convert unixstamp to time for string
-'''
-    return datetime.fromtimestamp(unixstamp).strftime('%Y-%m-%d %H:%M:%S')
+def unixstamp2strtime(unixstamp):
+    '''
+        convert unixstamp to time for string
 
+    '''
+    strtime=datetime.fromtimestamp(unixstamp).strftime('%Y-%m-%d %H:%M:%S')
+    return strtime
 
-def md5str(origin_str)
-'''
-    convert string to md5 digest
-'''
+def md5str(origin_str):
+    '''
+        convert string to md5 digest
+    '''
     return str(hashlib.md5(origin_str).hexdigest())
 
-def controller(db_name)
-conn = sqlite3.connect(db_name)
-chatlist = conn.execute("select * from sqlite_sequence")
-print "totally "+len(chatlist) "session"
+def controller(db_name):
+    conn = sqlite3.connect(db_name)
+    chatlist = conn.execute("select * from sqlite_sequence")
+    #print "totally "+len(chatlist)+" sessions"
 
 #get chat session done
 
 #conn.execute("")
-#for row in cursor:
-#	row[0]
+    i=0
+    for row in chatlist:
+        #print str(i)+":"+row[0]
+        i=i+1
 #	conn.close
-conn.close()
+    print str(i)
+    conn.close()
 
-def printhelp()
+def printhelp():
     print "usage: "+sys.argv[0]+" db_name"
 
-if len(sys.argc) == 2 :
+if len(sys.argv) == 2 :
         controller(sys.argv[1]);
-else
+else:
     printhelp();
