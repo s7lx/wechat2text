02f8e195b35c7bab4267b59624c9a1ef2bd62990
s7lx <s7lxhacker@gmail.com>
2017-04-15 20:59:09 +0800

    alpha version 0.1

--- run.py
+++ run.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python
+# encoding: utf-8
+
 import sqlite3
 import hashlib
 import sys
 import time
 from datetime import datetime
 
+def read_file(fn):
+    '''read file '''
+    fp=open(fn,"r")
+    h=fp.read()
+    fp.close
+    return h
+
+def write_file(fn,content):
+    '''read file '''
+    fp=open(fn,"wb")
+    h=fp.write(content)
+    fp.close
+    return h
+
 def unixstamp2strtime(unixstamp):
     '''
         convert unixstamp to time for string
 
     '''
-    strtime=datetime.fromtimestamp(unixstamp).strftime('%Y-%m-%d %H:%M:%S')
-    return strtime
+    return datetime.fromtimestamp(unixstamp).strftime('%Y-%m-%d %H:%M:%S')
+
 
 def md5str(origin_str):
     '''
@@ -19,26 +35,83 @@ def md5str(origin_str):
     '''
     return str(hashlib.md5(origin_str).hexdigest())
 
-def controller(db_name):
-    conn = sqlite3.connect(db_name)
-    chatlist = conn.execute("select * from sqlite_sequence")
-    #print "totally "+len(chatlist)+" sessions"
 
-#get chat session done
-
-#conn.execute("")
+def countlen(cursor):
     i=0
-    for row in chatlist:
-        #print str(i)+":"+row[0]
+    for row in cursor
         i=i+1
-#	conn.close
-    print str(i)
-    conn.close()
+    return i
+
+def resolve_message(content_type,content_type):
+    '''
+        todo:
+    '''
+    result=content
+    return result
+
+
+def build_session(file_buf,username,time,content_type,content,des):
+    '''
+        build one piece of message to text
+        format 1:   time -- me -- \n message
+        format 2:   username -- time  \n message
+    '''
+    user =username #single chat session
+
+    if des :
+        #format 2
+        file_buf = file_buf + user + unixstamp2strtime(time) + "\n"
+    else :
+        #format 1
+        file_buf = file_buf + unixstamp2strtime(time) + "我：" + "\n"
+
+    #resolve it
+    file_buf = file_buf + resolve_message(content_type,content) + "\n"
+
+    file_buf = file_buf+"\n"
+    return
+
+def output_data(db_handle,username):
+    '''
+    TODO:
+    '''
+    chat_db="Chat_"+md5str(username)
+    sql="select CreateTime,Type,Message,Des from "+chat_db+" order by MesLocalID "
+
+    chat_log = db_handle.execute(sql)
+
+    #prepare head of session
+    content="Total :"+str(countlen(chat_log)) + "messages"+ "\n\n"
+
+    for row in chat_log
+        build_session(content,username,row[0],row[1],row[2],row[3])
+
+    write_file(username,content)
+    return
+
+def controller(db_handle):
+    #count chat list
+    chatlistcount = db_handle.execute("select name from sqlite_sequence where name like \"Chat_%\"")
+    print "Total: "+str(countlen(chatlistcount))+" sessions"
+
+    #get chat list from table(Session list )
+    cur = db_handle.execute("select username from friend_meta order by lastUpdate desc")
+
+    #excute read data (from db) and write file (text)
+    for username in cur
+        output(db_handle,username[0])
+
+    return
 
 def printhelp():
-    print "usage: "+sys.argv[0]+" db_name"
+    print "usage: "+sys.argv[0]+" db_file_name [table_name]"
+    return
 
-if len(sys.argv) == 2 :
-        controller(sys.argv[1]);
+#main function
+if len(sys.argv) >1 :
+    conn=sqlite3.connect(sys.argv[1])
+    controller(conn)
+    conn.close()
+
 else:
     printhelp();
